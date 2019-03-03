4babd1583e8fda45d163b50751c93fe25e1c5937
rockatoo <youlia73@naver.com>
2019-03-03 12:23:55 +0900

    Added variables & functions
    Signed-off-by: rockatoo <youlia73@naver.com>

--- Source/BatteryCollector/Pickup.h
+++ Source/BatteryCollector/Pickup.h
@@ -23,6 +23,18 @@ public:
 	/** Reurn the mesh for the pickup */
 	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
 
+	/** Return whether or not the pickup is active */
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+	bool IsActive();
+
+	/** Allows other classes to safely change whether or not pickup is deactivated */
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void SetActive(bool PickupState);
+
+protected:
+	/** True when the pickup can be used, and false when pickup is deactivated */
+	bool bIsActive;
+
 private:
 	/** Staticmesh to represent the pickup in the level */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
