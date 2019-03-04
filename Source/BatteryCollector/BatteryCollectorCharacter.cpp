ce28797bbb61b076c619aa3f33ced306a37358c2
rockatoo <youlia73@naver.com>
2019-03-04 12:26:02 +0900

    Added changing character speed & material according to power level
    Signed-off-by: rockatoo <youlia73@naver.com>

--- Source/BatteryCollector/BatteryCollectorCharacter.cpp
+++ Source/BatteryCollector/BatteryCollectorCharacter.cpp
@@ -50,6 +50,10 @@ ABatteryCollectorCharacter::ABatteryCollectorCharacter()
 	// Set a base power level for the character
 	InitialPower = 2000.f;
 	CharacterPower = InitialPower;
+
+	// set the dependence of the speed on the power level
+	SpeedFactor = 0.75f;
+	BaseSpeed = 10.0f;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -190,7 +194,13 @@ float ABatteryCollectorCharacter::GetCurrentPower()
 	return CharacterPower;
 }
 
+// Called whenever power is increased or decreased
 void ABatteryCollectorCharacter::UpdatePower(float PowerChange)
 {
+	// change power
 	CharacterPower = CharacterPower + PowerChange;
+	// change speed based on power
+	GetCharacterMovement()->MaxWalkSpeed = BaseSpeed + SpeedFactor * CharacterPower;
+	// call visual effect
+	PowerChangeEffect();
 }
